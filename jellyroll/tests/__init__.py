edc8a134d1a99a1b4f4eeacdd37441dca99ce571
Jacob Kaplan-Moss <jacob@jacobian.org>
2008-10-07 12:05:12 -0500

    Removed outdated test_utils.

--- jellyroll/tests/__init__.py
+++ jellyroll/tests/__init__.py
@@ -1,4 +1,3 @@
 from test_items import *
 from test_tags import *
 from test_views import *
-from test_utils import *
--- jellyroll/tests/test_utils.py
+++
@@ -1,8 +0,0 @@
-from django.test import TestCase
-import jellyroll.utils
-
-# XXX this isn't much of a test.
-
-class TestHighlightCode(TestCase):
-    def testHighlightCode(self):
-        source = jellyroll.utils.highlight_code("def foo(): pass", language="python")
