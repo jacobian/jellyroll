a6f087a55a85b9394b623cb56fc8b2eb78e941ef
Jacob Kaplan-Moss <jacob@jacobian.org>
2008-10-23 10:50:02 -0600

    Updated Youtube provider to use new gdata API; this means that dates are actually correct

--- jellyroll/providers/youtube.py
+++ jellyroll/providers/youtube.py
@@ -1,14 +1,14 @@
 import datetime
 import logging
-import xmlrpclib
+import feedparser
 from django.conf import settings
 from django.db import transaction
-from django.utils.encoding import smart_unicode
+from django.utils.encoding import smart_unicode, smart_str
 from jellyroll.models import Item, VideoSource, Video
 from jellyroll.providers import utils
 
-# Isn't xmlrpclib nifty?
-youtube = xmlrpclib.Server("http://www.youtube.com/api2_xmlrpc").youtube
+TAG_SCHEME = 'http://gdata.youtube.com/schemas/2007/keywords.cat'
+FEED_URL = 'http://gdata.youtube.com/feeds/api/users/jacobkm/favorites'
 
 log = logging.getLogger("jellyroll.providers.youtube")
 
@@ -16,39 +16,57 @@ log = logging.getLogger("jellyroll.providers.youtube")
 # Public API
 #
 def enabled():
-    ok = hasattr(settings, "YOUTUBE_USERNAME") and hasattr(settings, "YOUTUBE_DEVELOPER_ID")
+    ok = hasattr(settings, "YOUTUBE_USERNAME")
     if not ok:
         log.warn('The Youtube provider is not available because the '
-                 'YOUTUBE_USERNAME and/or YOUTUBE_DEVELOPER_ID settings are '
-                 'undefined.')
+                 'YOUTUBE_USERNAME settings is undefined undefined.')
     return ok
 
-def update():
-    res = youtube.users.list_favorite_videos({
-        "dev_id" : settings.YOUTUBE_DEVELOPER_ID,
-        "user" : settings.YOUTUBE_USERNAME
-    })
-    xml = utils.ET.fromstring(res)
-    for video in xml.getiterator("video"):
-        title = smart_unicode(video.find("title").text)
-        url = smart_unicode(video.find("url").text)
-        tags = smart_unicode(video.find("tags").text).lower()
-        # suckage: youtube doesn't expose the date a movie was favorited :(
-        timestamp = datetime.datetime.now()
-        log.debug("Handling video: %r" % title)
-        _handle_video(title, url, tags, timestamp)
-        
+def update():    
+    start_index = 1
+    max_results = 50
+    while True:
+        log.debug("Fetching videos %s - %s" % (start_index, start_index+max_results-1))
+        feed = feedparser.parse(FEED_URL + '?v=2&start-index=%s&max-results=%s' % (start_index, max_results))
+        for entry in feed.entries:            
+            if 'link' in entry:
+                url = entry.link
+            elif 'yt_videoid' in entry:
+                url = 'http://www.youtube.com/watch?v=%s' % entry.yt_videoid
+            else:
+                log.error("Video '%s' appears to have no link" % (entry.tite))
+                continue
+                
+            _handle_video(
+                title = entry.title, 
+                url = url,
+                tags = " ".join(t['term'] for t in entry.tags if t['scheme'] == TAG_SCHEME),
+                timestamp = datetime.datetime(*entry.published_parsed[:6]),
+            )
+        if len(feed.entries) < max_results:
+            log.debug("Ran out of results; finishing.")
+            break
+            
+        start_index += max_results
 #
 # Private API
 #
 
+@transaction.commit_on_success
 def _handle_video(title, url, tags, timestamp):
+    log.debug("Handling video: %s" % smart_str(title))
     source = VideoSource.objects.get(name="YouTube")
-    vid, created = Video.objects.get_or_create(url=url, defaults={'title': title, 'source': source})
-    return Item.objects.create_or_update(
-        instance = vid, 
-        timestamp = timestamp,
-        tags = tags,
-        source = __name__,
+    vid, created = Video.objects.get_or_create(
+        url = url, 
+        defaults = {
+            'title': smart_unicode(title), 
+            'source': source
+        }
     )
-_handle_video = transaction.commit_on_success(_handle_video)
+    if created:
+        return Item.objects.create_or_update(
+            instance = vid, 
+            timestamp = timestamp,
+            tags = tags,
+            source = __name__,
+        )
