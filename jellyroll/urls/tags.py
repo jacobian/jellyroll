283be586c7e3ecaaa9c9714c0590cd413101864d
Jeremy Helms <digitaljhelms@gmail.com>
2009-02-28 23:21:41 -0600

    Added support for the colon (":") character in the tag escape pattern; without this, Delicious links tagged for other people ("for:digitaljhelms") throw an exception during render.

--- jellyroll/urls/tags.py
+++ jellyroll/urls/tags.py
@@ -5,5 +5,5 @@ from jellyroll.views import tags
 urlpatterns = patterns('',
 
     url(r'^$', tags.tag_list, {}, name='jellyroll_tag_list'),
-    url(r'^(?P<tag>[-\.\'\w]+)/$',tags.tag_item_list, {}, name="jellyroll_tag_item_list"),
+    url(r'^(?P<tag>[-\.\'\:\w]+)/$',tags.tag_item_list, {}, name="jellyroll_tag_item_list"),
 )
