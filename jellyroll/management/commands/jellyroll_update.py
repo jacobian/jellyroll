75d7c58941b460cefecdee7b0244a0b7f43865d5
Jacob Kaplan-Moss <jacob@jacobian.org>
2009-03-12 13:45:13 -0500

    Remove the -v option which is now builtin.

--- jellyroll/management/commands/jellyroll_update.py
+++ jellyroll/management/commands/jellyroll_update.py
@@ -28,7 +28,11 @@ class Command(BaseCommand):
     )
     
     def handle(self, *args, **options):
-        level = {'0': logging.WARN, '1': logging.INFO, '2': logging.DEBUG}[options['verbosity']]
+        level = {
+            '0': logging.WARN, 
+            '1': logging.INFO, 
+            '2': logging.DEBUG
+        }[options.get('verbosity')['0']]
         logging.basicConfig(level=level, format="%(name)s: %(levelname)s: %(message)s")
 
         if options['list_providers']:
