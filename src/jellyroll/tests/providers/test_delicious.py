6a9c1c163a46e914711f8db76581842b90de1eee
Jacob Kaplan-Moss <jacob@jacobian.org>
2009-08-19 16:20:46 -0500

    Fairly good tests for the delicious provider. Looks like this technique for testing providers will work after all.

--- src/jellyroll/tests/providers/test_delicious.py
+++ src/jellyroll/tests/providers/test_delicious.py
@@ -1,8 +1,11 @@
 import mock
+import datetime
 import unittest
 from django.conf import settings
 from django.test import TestCase
+from jellyroll.models import Item, Bookmark
 from jellyroll.providers import delicious
+from jellyroll.providers.utils.anyetree import etree
 
 class DeliciousClientTests(unittest.TestCase):
     
@@ -27,8 +30,68 @@ class DeliciousClientTests(unittest.TestCase):
             username = 'username',
             password = 'password'
         )
-    
+
+#    
+# Fake delicious client that mocks all the calls update() makes.
+#
+def xml(s):
+    """Quick 'n' dirty XML etree maker"""
+    return etree.fromstring(s.strip())
+
+FakeClient = mock.Mock()
+
+# This makes FakeClient.__init__ do the right thing w/r/t mocking
+FakeClient.return_value = FakeClient
+
+FakeClient.posts.update.return_value = xml(
+    '<update time="2009-08-18T15:30:16Z" inboxnew="0"/>'
+)
+
+FakeClient.posts.dates.return_value = xml('''
+    <dates tag="" user="jellyroll">
+        <date count="1" date="2009-08-18"/>
+    </dates>
+''')
+
+FakeClient.posts.get.return_value = xml('''
+    <posts user="jellyroll" dt="2009-08-18T15:30:16Z">
+        <post href="http://jacobian.org/"
+              hash="151ebb66839faa8ed073b27fb897b166"
+              description="Me!"
+              time="2009-08-18T15:30:16Z"
+              extended="I'm awesome."
+              tag="me jacob jacobian"
+        />
+    </posts>
+''')
+
 class DeliciousProviderTests(TestCase):
     
     def test_enabled(self):
         self.assertEqual(delicious.enabled(), True)
+        
+    @mock.patch_object(delicious, 'DeliciousClient', FakeClient)
+    def test_update(self):
+        delicious.update()
+        
+        # Check that the calls to the API match what we expect
+        FakeClient.assert_called_with(settings.DELICIOUS_USERNAME, settings.DELICIOUS_PASSWORD)
+        FakeClient.posts.update.assert_called_with()
+        FakeClient.posts.dates.assert_called_with()
+        FakeClient.posts.get.assert_called_with(dt='2009-08-18')
+        
+        # Check that the bookmark exists
+        b = Bookmark.objects.get(url="http://jacobian.org/")
+        self.assertEqual(b.description, "Me!")
+        
+        # Check that the Item exists
+        i = Item.objects.get(content_type__model='bookmark', object_id=b.pk)
+        self.assertEqual(i.timestamp.date(), datetime.date(2009, 8, 18))
+        self.assertEqual(i.tags, 'me jacob jacobian')
+    
+    @mock.patch_object(delicious, 'DeliciousClient', FakeClient)
+    @mock.patch_object(delicious, 'log')
+    def test_update_skipped_second_time(self, mocked):
+        delicious.update()
+        delicious.update()
+        self.assert_(mocked.info.called)
